bbaa7cbac4e7294fa2054a251af63362c28d1544
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-06-25 13:31:06 -0500

    design the login form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,61 +49,70 @@
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <!-- Email and Password -->
-                            <form class="navbar-form navbar-left" role="form" method="POST" action="{{ url('/login') }}">
-                                {{ csrf_field() }}
-
-                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                    <input id="email" type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}"  autocomplete="email" required autofocus>
-                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password" required autocomplete="current-password">
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif    
-                                </div>
-                                <button type="submit" class="btn btn-default btn-lg btn-cta">Login</button>
-                            </form>
+                    @if ($_SERVER['REQUEST_URI'] =="/login")
+                        <ul class="nav navbar-nav navbar-right">
                             <div>
-                                <a class="btn btn-link pull-right" href="{{ url('/password/reset') }}">
+                                <a class="btn btn-link pull-right login-forgot" href="{{ url('/password/reset') }}">
                                     Forgot Password?
                                 </a>
                             </div>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->full_name }} <span class="caret"></span>
-                                </a>
-                                
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                                        Logout
-                                        </a>
-                                        
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                </ul>
-            </div>
+                        </ul>
+                    @else
+                    <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right">
+                            
+                            <!-- Authentication Links -->
+                            @if (Auth::guest())
+                                <!-- Email and Password -->
+                                <form class="navbar-form navbar-left" role="form" method="POST" action="{{ url('/login') }}">
+                                    {{ csrf_field() }}
+
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <input id="email" type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}"  autocomplete="email" required autofocus>
+                                        <input id="password" type="password" class="form-control" name="password" placeholder="Password" required autocomplete="current-password">
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif    
+                                    </div>
+                                    <button type="submit" class="btn btn-default btn-lg btn-cta">Login</button>
+                                </form>
+                                <div>
+                                    <a class="btn btn-link pull-right" href="{{ url('/password/reset') }}">
+                                        Forgot Password?
+                                    </a>
+                                </div>
+                            @else
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        {{ Auth::user()->full_name }} <span class="caret"></span>
+                                    </a>
+                                    
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li>
+                                            <a href="{{ url('/logout') }}"
+                                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                            Logout
+                                            </a>
+                                            
+                                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </li>
+                            @endif
+                        </ul>
+                    @endif
+                </div>
             </div>
         </nav>
 
