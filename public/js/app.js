1c5f70d5aec818d13b5c350eb914dcfa40fcdb4d
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-08-01 16:19:05 -0500

    change User title

--- public/js/app.js
+++ public/js/app.js
@@ -19155,7 +19155,7 @@ var render = function() {
                   attrs: { slot: "header" },
                   slot: "header"
                 },
-                [_vm._v("Add New")]
+                [_vm._v("Add New User")]
               )
             ]
           )
