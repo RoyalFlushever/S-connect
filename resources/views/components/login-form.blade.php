4f3eac5b80edecbac8a98db72fa7be25b85ec2f3
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-08-22 22:44:09 -0500

    style login page

--- resources/views/components/login-form.blade.php
+++ resources/views/components/login-form.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-md-offset-3 text-center welcome">
-            Welcome to the I-Connect project managementsite <br>
+            Welcome to the I-Connect project management site <br>
             You must login in to use this application. If you do not have a username 
             and password, please click here to <a href="/welcome" class="register">register</a>.
         </div>
@@ -10,7 +10,7 @@
             {{ csrf_field() }}
 
             <div class="form-group{{ $errors->has($username) ? ' has-error' : '' }}">
-                <div class="col-md-4 col-md-offset-4">
+                <div class="col-md-4 col-md-offset-4" style="padding-top: 40px; padding-bottom: 0px;">
                     <input id="{{ $username }}" type="{{ $username === 'email' ? 'email' : 'text' }}" class="form-control input-green" name="{{ $username }}" value="{{ old($username) }}" placeholder="USERNAME" required autofocus>
 
                     @if ($errors->has($username))
@@ -22,7 +22,7 @@
             </div>
 
             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <div class="col-md-4 col-md-offset-4">
+                <div class="col-md-4 col-md-offset-4" style="padding-top: 0px;">
                     <input id="password" type="password" class="form-control input-blue" name="password" placeholder="PASSWORD" required>
 
                     @if ($errors->has('password'))
